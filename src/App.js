665d214c8526ccff491846e1ff565ef3a751f8c2
Pham <Brian.D.Pham@gov.bc.ca>
2022-11-14 16:55:24 -0800

    added pop up styling and trailer iframe

--- src/App.js
+++ src/App.js
@@ -1,107 +1,103 @@
-import {useState,useEffect} from 'react';
-import AnimeCard from './components/AnimeCard';
-import SearchIcon from './icons/search.svg'
-import './App.css';
-import './assets/searchbar.scss'
+import { useState, useEffect } from "react";
+import AnimeCard from "./components/AnimeCard";
+import SearchIcon from "./icons/search.svg";
+import "./App.css";
+import "./assets/searchbar.scss";
 
-const API_URL = 'https://api.jikan.moe/v4'
+const API_URL = "https://api.jikan.moe/v4";
 
-
-const App=()=> {
-  const[animes,setAnimes] = useState([]);
-  const[searchTerm,setSearchTerm] = useState('');
-  const[currentPage,setCurrentPage] = useState(1);
-  const[hasNextPage,setHasNextPage] = useState(false);
-
-
-
+const App = () => {
+  const [animes, setAnimes] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [hasNextPage, setHasNextPage] = useState(false);
 
   const searchAnime = async (search) => {
-      const response = await fetch(`${API_URL}/anime?q=${search}&limit=10&page=${currentPage}`);
-      const data = await response.json();
-      setAnimes([])
-      setAnimes([data.data]);
-      console.log(data.data[0]);
-      setHasNextPage(data.pagination.has_next_page);
-  }
+    const response = await fetch(
+      `${API_URL}/anime?q=${search}&limit=10&page=${currentPage}`
+    );
+    const data = await response.json();
+    setAnimes([]);
+    setAnimes([data.data]);
+    console.log(data.data);
+    setHasNextPage(data.pagination.has_next_page);
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     searchAnime(searchTerm);
-
   }, [currentPage]);
 
   return (
     <div className="place-content-center text-textcolor flex flex-col justify-center items-center">
-      <h1>Anime DB</h1>
-
-      <div class="s-container">
-        <input 
-          className='search-field'
-          type="text" 
-          placeholder="Input anime name...."
-          value={searchTerm}
-          onChange = {(e) => setSearchTerm(e.target.value)}
-        
-        />
-        <div 
-          class="search"
-          onClick={()=>{searchAnime(searchTerm.replace(/ /g, '%20'));setCurrentPage(1)}}
+      <div className="sticky flex flex-row h-20 w-full bg-cardbg top-0 z-40">
+        <h1
+          className="text-5xl cursor-pointer p-3"
+          onClick={() => {
+            setCurrentPage(1);
+            searchAnime('');
+          }}
         >
-          <img
-            src={SearchIcon} alt="Search Icon"
+          Anime DB
+        </h1>
+        <div className="ml-auto flex flex-row p-2">
+          <input
+            className="search-field"
+            type="text"
+            placeholder="Input anime name...."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-        </div>
-      </div>
-      <div className='container px-5 py-2 mx-auto justify-center'>
-        {
           
-          animes?.length>0
-          ? (
-            <div className='justify-center items-center flex flex-wrap -m-1 md:-m-2'>
-              {animes[0].map((anime)=>(
-                <AnimeCard anime={anime}/>
-              ))}
-            </div>
-          ) : (
-            <div className='empty'> 
-              <h2>No search results.</h2>
-            </div>
-          )
-        }
+        </div>
+        <div
+            className="w-20 p-3 cursor-pointer bg-gray-500 hover:bg-gray-400"
+            onClick={() => {
+              searchAnime(searchTerm.replace(/ /g, "%20"));
+              setCurrentPage(1);
+            }}
+          >
+            <img src={SearchIcon} alt="Search Icon" />
+          </div>
       </div>
-      <div className='inline-flex'>
-        {
-            currentPage<2 
-        ? (
-            null
-            ):(
-            <button 
-            className='bg-cardbg hover:bg-cardbghover text-textcolor font-bold py-2 px-4 rounded-l' 
-            onClick={()=>setCurrentPage(currentPage-1)}>Prev</button>
-            )
-        }
-        
-            <div className='bg-cardbg text-textcolor font-bold py-2 px-4 border-r border-l border-bgcolor'>
-              <p>{currentPage}</p>
-            </div>
 
-        {
-            hasNextPage===false 
-        ? (
-            null
-            ):(
-            
-              <button
-                className='bg-cardbg hover:bg-cardbghover text-textcolor font-bold py-2 px-4 rounded-r'
-                onClick={()=>setCurrentPage(currentPage+1)}
-              >Next</button>
-            )
-        }
+      <div className="container px-5 py-2 mx-auto justify-center">
+        {animes?.length > 0 ? (
+          <div className="justify-center items-center flex flex-wrap -m-1 md:-m-2">
+            {animes[0].map((anime) => (
+              <AnimeCard anime={anime} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No search results.</h2>
+          </div>
+        )}
       </div>
-      
+      <div className="inline-flex">
+        {currentPage < 2 ? null : (
+          <button
+            className="bg-cardbg hover:bg-cardbghover text-textcolor font-bold py-2 px-4 rounded-l"
+            onClick={() => setCurrentPage(currentPage - 1)}
+          >
+            Prev
+          </button>
+        )}
 
+        <div className="bg-cardbg text-textcolor font-bold py-2 px-4 border-r border-l border-bgcolor">
+          <p>{currentPage}</p>
+        </div>
+
+        {hasNextPage === false ? null : (
+          <button
+            className="bg-cardbg hover:bg-cardbghover text-textcolor font-bold py-2 px-4 rounded-r"
+            onClick={() => setCurrentPage(currentPage + 1)}
+          >
+            Next
+          </button>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/AnimeCard.jsx
+++ src/components/AnimeCard.jsx
@@ -1,34 +1,48 @@
 
 import React, { useState } from 'react';
 import Popup from 'reactjs-popup';
+import Iframe from 'react-iframe';
 import 'reactjs-popup/dist/index.css';
 
 const AnimeCard = ({anime}) => {
   const [open, setOpen] = useState(false);
   const closeModal = () => setOpen(false);
     return (
-        <div onClick={() => setOpen(o => !o)} className='flex flex-col bg-cardbg hover:bg-cardbghover w-64 justify-center items-center p-2 m-4 rounded-lg cursor-pointer'>
+        <div onClick={() => setOpen(o => !o)} className='flex flex-col bg-cardbg hover:bg-cardbghover drop-shadow-md w-64 justify-center items-center m-4 rounded-lg cursor-pointer'>
           {/* the popup */}
           <Popup open={open} closeOnDocumentClick onClose={closeModal}>
-            <div className="modal rounded-lg shadow">
-              <div className='flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600'>
+            <div>
+              <div className='flex justify-between items-start rounded-t border-b dark:border-gray-600'>
                 <h3 class="text-xl font-semibold text-gray-900">
-                  Description
+                  Info
                 </h3>
-                <button className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal" onClick={closeModal}>
-                  <p className='w-5 h-5'>&times;</p>
+                <button className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 text-sm ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal" onClick={closeModal}>
+                  <p className='w-6 h-6'>&times;</p>
                 </button>
               </div>
+              <div className='h-2/3 overflow-y-auto p-6 bg-bgcolor text-textcolor'>
+                <p><b>Japanese tittle:</b> {anime.title_japanese} ({anime.title} ) </p>
+                <p><b>English tittle:</b> {anime.title_english}</p>
+                <h4><b>Description:</b></h4>
+                <p>{anime.synopsis}</p>
+                <p><b>Trailer:</b></p>
+                {anime.trailer.embed_url ? 
+                  <Iframe url={anime.trailer.embed_url}
+                  width="100%"
+                  height="320px"
+                  display="block"
+                  position="relative"/>
+                   : <p>Unavailable</p>}
+              </div>
               
-              <p>{anime.synopsis}</p>
             </div>
           </Popup>
           {/* remaining content of card */}
-          <div>
-            <img src={anime.images.webp.image_url} alt={anime.title}/>
+          <div >
+            <img className='w-64 rounded-t-lg' src={anime.images.webp.image_url} alt={anime.title}/>
           </div>
           <div className="p-6 mb-2">
-            <h2 className="text-xl font-medium">{anime.title}</h2>
+            <h2 className="text-xl font-bold">{anime.title}</h2>
             <p>{anime.type}</p>
             <p>{anime.aired.string}</p>
           </div>
